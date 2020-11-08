60d5e1a7946f3fb5c12a3d97eac8555be1915cdd
Roumesh <rpersand25@gmail.com>
2020-11-08 16:17:00 -0500

    Updates

---
+++ precache-manifest.104d6a34284ca2e98a0d0b63c4c4175c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a6f1c65c35f813b957bd99c3a4e935",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f0333b606a3364e62a6",
+    "url": "/monsters-rolodex/static/css/main.b39c1d55.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0333b606a3364e62a6",
+    "url": "/monsters-rolodex/static/js/main.305958fb.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
